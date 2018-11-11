c34cc4c0376459c15e67349d5228276606331504
Your Name <you@example.com>
2018-11-11 19:00:16 -0300

    buscador tapas

--- src/_modules/slider/slider.js
+++ src/_modules/slider/slider.js
@@ -4,6 +4,8 @@
 var Slider = function() {
     var slider = $('._slider');
     var sliderMulti = $('._slidermulti');
+    var sliderCover = $('._slidercover');
+
     if (slider) {
         slider.each(function(){
             $(this).slick({
@@ -23,12 +25,44 @@ var Slider = function() {
                 slidesToShow: 3,
                 slidesToScroll: 1,
                 centerMode: true,
-                autoplay: true,
+                autoplay: false,
                 responsive: [
                     {
                         breakpoint: 900,
                         settings: {
                             slidesToShow: 1,
+                            slidesToScroll: 1,
+                            centerMode: true,
+                        }
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: {
+                            centerMode: true,
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
+                    }
+                ]
+            });
+        });
+    }
+
+    if (sliderCover) {
+        sliderCover.each(function(){
+            $(this).slick({
+                dots: true,
+                infinite: true,
+                speed: 600,
+                slidesToShow:5,
+                slidesToScroll: 1,
+                centerMode: true,
+                autoplay: false,
+                responsive: [
+                    {
+                        breakpoint: 900,
+                        settings: {
+                            slidesToShow: 3,
                             slidesToScroll: 1,
                             centerMode: true,
                         }
